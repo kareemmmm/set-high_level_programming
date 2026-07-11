fe58ea5f316aa0daf11972b2eeb282d52add0e14
kareemmmm <kareemmattr1994@gmail.com>
2026-07-11 04:09:02 +0300

    fixing 1-0check_cycle.c

--- python-hello_world/10-check_cycle.c
+++ python-hello_world/10-check_cycle.c
@@ -3,6 +3,7 @@
 /**
  * check_cycle - checks if a singly linked list has a cycle in it
  * @list: pointer to the head of the list
+ *
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
@@ -10,14 +11,21 @@ int check_cycle(listint_t *list)
 	listint_t *slow = list;
 	listint_t *fast = list;
 
-	while (slow && fast && fast->next)
-	{
-		slow = slow->next;
-		fast = fast->next->next;
+	/* If the list is empty or has only one element, it can't have a cycle */
+	if (list == NULL)
+		return (0);
 
+	/* Traverse the list with two pointers at different speeds */
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;          /* moves 1 step at a time */
+		fast = fast->next->next;    /* moves 2 steps at a time */
+
+		/* If the fast pointer catches up to the slow pointer, a cycle exists */
 		if (slow == fast)
 			return (1);
 	}
 
+	/* If fast reaches the end (NULL), there is no cycle */
 	return (0);
 }
