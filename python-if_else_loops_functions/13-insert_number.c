846b7f540944dfb1f2a7df002e40cb1ece3be3ce
kareemmmm <kareemmattr1994@gmail.com>
2026-07-11 05:10:59 +0300

    adding the python-fi_else_loops_fuctions project

---
+++ python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a number into a sorted singly linked list.
+ * @head: Double pointer to the head of the linked list.
+ * @number: The number to insert into the list.
+ * Return: Address of the new node, or NULL if it failed.
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new_node;
+    listint_t *current;
+
+    if (head == NULL)
+        return (NULL);
+
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return (NULL);
+
+    new_node->n = number;
+    new_node->next = NULL;
+
+    current = *head;
+
+    // Insert at the beginning if list is empty or number is smaller than head
+    if (current == NULL || current->n >= number)
+    {
+        new_node->next = current;
+        *head = new_node;
+        return (new_node);
+    }
+
+    // Traverse to find correct placement position
+    while (current->next != NULL && current->next->n < number)
+    {
+        current = current->next;
+    }
+
+    new_node->next = current->next;
+    current->next = new_node;
+
+    return (new_node);
+}
---
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
