61fa0f72b662023204539a9c17660021810e8cb7
kareemmmm <kareemmattr1994@gmail.com>
2026-08-08 14:17:34 +0300

    0-add_integer has been fixed

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -16,4 +16,12 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    try:
+        a = int(a)
+    except (OverflowError, ValueError):
+        raise TypeError("a must be an integer")
+    try:
+        b = int(b)
+    except (OverflowError, ValueError):
+        raise TypeError("b must be an integer")
+    return a + b
